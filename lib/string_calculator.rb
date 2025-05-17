f501ea9b715b0a5ccfca34bd95708c3a83b0f178
Ankur Saini <ankur@metadesignsolutions.co.uk>
2025-05-17 20:57:30 +0530

    Step 3: Handle custom delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -10,5 +10,21 @@ class Calculator
     # Step 2: Handle single number
     return string.to_i if !string.include?(',') && !string.include?("\n") && !string.start_with?('//')
 
+    # Step 3: Handle custom delimiter
+    delimiters = [',', "\n"]
+    if string.start_with?('//')
+      delimiter_end = string.index("\n")
+      raise ArgumentError, "Invalid delimiter format" if delimiter_end.nil?
+      
+      delimiter_section = string[2...delimiter_end]
+      if delimiter_section.start_with?('[') && delimiter_section.end_with?(']')
+        delimiters = delimiter_section.scan(/\[(.*?)\]/).flatten
+      else
+        delimiters = [delimiter_section]
+      end
+      
+      string = string[(delimiter_end + 1)..]
+    end
+
   end
 end
