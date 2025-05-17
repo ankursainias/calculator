43a2b2ab402e428360f09017f3410db29bbe57b9
Ankur Saini <ankur@metadesignsolutions.co.uk>
2025-05-17 20:34:10 +0530

    Step 2: Handle single number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -7,5 +7,8 @@ class Calculator
     # Step 1: Handle empty string
     return 0 if string.empty?
 
+    # Step 2: Handle single number
+    return string.to_i if !string.include?(',') && !string.include?("\n") && !string.start_with?('//')
+
   end
 end
