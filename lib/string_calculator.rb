519cc1abf531eba8ead2e83d734c916ea130b595
Ankur Saini <ankur@metadesignsolutions.co.uk>
2025-05-17 20:26:38 +0530

    Step 1: Handle empty string

---
+++ lib/string_calculator.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Calculator
+  class NegativeNumberError < StandardError; end
+
+  def add(string)
+    # Step 1: Handle empty string
+    return 0 if string.empty?
+
+  end
+end
