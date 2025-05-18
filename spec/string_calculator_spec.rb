845a3ecc555f01b62c62111b6781385ab023deff
Ankur Saini <ankur@metadesignsolutions.co.uk>
2025-05-18 17:39:23 +0530

    test: example for add numbers with newline delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -20,5 +20,9 @@ RSpec.describe StringCalculator do
       expect(calculator.add('1,2')).to eq(3)
     end
 
+    # 4. add new line
+    it 'returns the sum of two numbers with new line' do
+      expect(calculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
