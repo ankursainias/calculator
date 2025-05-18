6e914cb2ef975cee6e08151a77c00f0ac47b42af
Ankur Saini <ankur@metadesignsolutions.co.uk>
2025-05-18 17:25:38 +0530

    test: example for empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe StringCalculator do
   subject(:calculator) { described_class.new }
 
   describe '#add' do
+    # 1. empty string
+    it 'returns 0 for an empty string' do
+      expect(calculator.add('')).to eq(0)
+    end    
   end
 end
